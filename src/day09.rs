2d29bb4e4924d5757f1123f7c6917cfb310da3d1
Łukasz Piwowar <zsacul@gmail.com>
2022-12-09 16:42:30 +0100

    day09

--- src/day09.rs
+++ src/day09.rs
@@ -8,12 +8,11 @@ struct Vec2{
 
 impl Vec2 {
     fn new(x:i64,y:i64)->Vec2 {
-        Self {
-            x,
-            y
-        }
+        Self { x, y }
     }
 }
+
+#[derive(Clone)]
 struct Range{
     head : Vec2,
     tail : Vec2,
@@ -23,102 +22,86 @@ struct Range{
 impl Range {
     fn new()->Self {
         Self {
-            head: Vec2::new(0,0),
-            tail: Vec2::new(0,0),
-            pos : HashSet::new(),
+            head : Vec2::new(0,0),
+            tail : Vec2::new(0,0),
+            pos  : HashSet::new(),
         }
     }
 
     fn move_tail(&mut self)
     {
-        let dx = self.head.x-self.tail.x;
-        let dy = self.head.y-self.tail.y;
-
-        if dx==0 || dy==0
-        {
-            if dx>0 { self.tail.x+=1 }
-            if dx<0 { self.tail.x-=1 }
-            if dy>0 { self.tail.y+=1 }
-            if dy<0 { self.tail.y-=1 }
-        }
-        else
-        {
-            if dx>0 { self.tail.x+=1 }
-            if dx<0 { self.tail.x-=1 }
-            if dy>0 { self.tail.y+=1 }
-            if dy<0 { self.tail.y-=1 }
-        }
+        self.tail.x+=(self.head.x - self.tail.x).signum();
+        self.tail.y+=(self.head.y - self.tail.y).signum();
         
+        self.pos.insert( (self.tail.x,self.tail.y) );
     }
 
     fn touches(&self)->bool
     {
-        let dx = self.head.x-self.tail.x;
-        let dy = self.head.y-self.tail.y;
-        !(dx.abs()>1 || dy.abs() >1)
-//        dx.abs() + dy.abs() <=1
+        (self.head.x-self.tail.x).abs()<=1 && 
+        (self.head.y-self.tail.y).abs()<=1
     }
 
-    fn moveme(&mut self,c:char,moves:usize)
-    {
-        //let movesp = [(-1, 0),( 1, 0),( 0,-1),( 0, 1)];
-        self.pos.insert( (self.tail.x,self.tail.y) );
-        
-        println!("{} {}",c,moves);
-
-        for i in 0..moves
-        {
-            match  c  {
-                'L' => { self.head.x-=1; },
-                'R' => { self.head.x+=1; },
-                'U' => { self.head.y-=1; },
-                'D' => { self.head.y+=1; },
-                _   => { panic!("wrong code")},
-            }
-        }
-
-        while (!self.touches())
-        {
-            self.move_tail();            
-            self.pos.insert( (self.tail.x,self.tail.y) );
-        }
-           
-        //while (!self.touches()) {
-             
-        //}
-        
-    }
-
-    //<6636
-    //1856
     fn movestr(&mut self,s:&str)
     {
-//        let positions = [( 0, 0),( 1,-1),( 0,-1),( 0, 1),(-1, 1),( 1, 0),(-1, 0)];
-        let tab   : Vec<&str> =  s.split(' ').collect(); 
-        let dir = tab[0].chars().next().unwrap();
-        let moves = tab[1].parse::<usize>().unwrap();
-        self.moveme(dir,moves);
-
+        let (dir,moves) = Self::get_moves(s);
+        self.move_head(dir,moves);
     }
 
+    fn get_moves(s:&str)->(char,usize)
+    {
+        let tab : Vec<&str> = s.split(' ').collect(); 
+        let dir   = tab[0].chars().next().unwrap();
+        let moves = tab[1].parse::<usize>().unwrap();
+        (dir,moves)
+    }
+
+    fn move_head(&mut self,c:char,moves:usize)
+    {
+        for _i in 0..moves
+        {
+            match  c  {
+                'L' => { self.head.x-=1;      },
+                'R' => { self.head.x+=1;      },
+                'U' => { self.head.y-=1;      },
+                'D' => { self.head.y+=1;      },
+                _   => { panic!("wrong code") },
+            }
+        }
+
+        let v = self.head;
+        self.set_head_pos(&v);
+    }
+
+    fn set_head_pos(&mut self,p:&Vec2)
+    {
+        self.head.x = p.x;
+        self.head.y = p.y;
+    }
+
+    fn follow(&mut self)->bool
+    {
+        self.pos.insert( (self.tail.x,self.tail.y) );
+        let moved = !self.touches();
+
+        if moved
+        {
+            self.move_tail();            
+        }  
+        moved            
+    }
+
+    #[allow(unused)]
     fn print(&self)
     {
-        for y in -10..5 {
-            for x in -4..15 {
+        for y in -30..35 {
+        for x in -24..25 {
                 let v = self.pos.contains(&(x,y));
 
-                if v 
-                {
-                    print!("#");
-                }
-                else
-                {
-                    print!(".");
-                }
-                
+                if v { print!("#"); }
+                else { print!("."); }
             }
-            println!("");
-            
+            println!();
         }
     }
 
@@ -128,27 +111,75 @@ impl Range {
     }
 }
 
-
-
-pub fn part1(data:&[String])->i32
+pub fn part1(data:&[String])->usize
 {
-    let mut r = Range::new();    
-    for s in data.iter() {
-        r.movestr(&s[..]);  
-    } 
-    r.print();
-    r.count() as i32
+    let mut r = Range::new();
 
-      //         .filter(|r| r.contains())
-        //       .count() as i32
+    for s in data.iter() 
+    {
+        r.movestr(&s[..]);  
+        while r.follow() {};
+    } 
+   
+    r.count()
 }
 
-pub fn part2(data:&[String])->i32
+#[allow(unused)]
+fn debug_print(r:&Vec<Range>)
 {
-    let mut r = Range::new();    
-    data.iter().map(|s| r.movestr(&s[..]));
+    let n = r.len();
+    let s = 16;
+    for y in -s..s 
+    {
+        for x in -s..s 
+        {
+            let mut was = false;
+
+            for q in 0..n
+            {
+                for (q, rq) in r.iter().enumerate().take(n)
+                {
+                    if rq.head.x==x && rq.head.y==y
+                    {
+                        was = true;
+                        if q==0 { print!("H");    }
+                           else { print!("{}",q); }
+                        break;
+                    }    
+                }                
+            }
+            if !was {
+                print!(".");
+            }
+        }
+        println!();
+    }        
+}
+
+pub fn part2(data:&[String])->usize
+{
+    let n = 9;
+    let mut r = vec![Range::new(); n];
+
+    for s in data.iter() 
+    {
+        let (move_des,count) = Range::get_moves(&s[..]);  
+
+        for _ in 0..count
+        {
+            r[0].move_head(move_des,1);
+            r[0].follow();
+
+            for i in 1..n
+            {
+                let tail_pos = r[i-1].tail;
+                r[i].set_head_pos(&tail_pos);
+                r[i].follow();
+            }
+        }     
+    }
     
-    r.count() as i32
+    r.last().unwrap().count()
 }
 
 #[allow(unused)]
@@ -179,6 +210,46 @@ fn test1()
 fn test2()
 {
     let v = vec![
+        "R 5".to_string(),
+        "U 8".to_string(),
+        "L 8".to_string(),
+        "D 3".to_string(),
+        "R 17".to_string(),
+        "D 10".to_string(),
+        "L 25".to_string(),
+        "U 20".to_string(),
     ];
-    assert_eq!(part2(&v),4);
+    assert_eq!(part2(&v),36);
+}
+
+#[test]
+fn test2_2()
+{
+    let v = vec![
+        "R 4".to_string(),
+        "U 4".to_string(),
+        "L 3".to_string(),
+        "D 1".to_string(),
+        "R 4".to_string(),
+        "D 1".to_string(),
+        "L 5".to_string(),
+        "R 2".to_string(),
+    ];
+    assert_eq!(part2(&v),1);
+}
+
+#[test]
+fn test2_3()
+{
+    let v = vec![
+        "R 5".to_string(),
+        "U 8".to_string(),
+        "L 8".to_string(),
+        "D 3".to_string(),
+        "R 17".to_string(),
+        "D 10".to_string(),
+        "L 25".to_string(),
+        "U 20".to_string(),
+    ];
+    assert_eq!(part2(&v),36);
 }
