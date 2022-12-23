c51b47843ab1a8049be86e637c23a8757a7a4b90
Łukasz Piwowar <zsacul@gmail.com>
2022-12-23 08:24:42 +0100

    t1

--- src/day23.rs
+++ src/day23.rs
@@ -2,31 +2,35 @@ use std::collections::HashMap;
 use super::vec2::Vec2;
 use super::tools;
 
-#[derive( Eq, PartialEq, Debug,  Clone)]
+#[derive(Eq, PartialEq, Debug, Clone)]
 struct Elf
 {
     id      : usize,
     pos     : Vec2,
     moves   : Vec<char>,
     move_to : Vec2,
-    move_ok : bool
+    move_ok : bool,
+    nothing : bool
 }
 
-impl Elf {
+impl Elf 
+{
     fn new(pos:Vec2,id:usize)->Self
     {
-        Self {
+        Self 
+        {
             id,
             pos,
-            moves  : vec!['n','s','w','e'],
-            move_to: Vec2::zero(),
-            move_ok: false
+            moves   : vec!['n','s','w','e'],
+            move_to : Vec2::zero(),
+            move_ok : false,
+            nothing : false
         }
     }
 
     fn shift_moves(&mut self)
     {
-        let t = self.moves[0];
+        let         t = self.moves[0];
         self.moves[0] = self.moves[1];
         self.moves[1] = self.moves[2];
         self.moves[2] = self.moves[3];
@@ -48,13 +52,13 @@ impl World {
     {
         self.field.clear();
 
-        for elf in self.elfs
+        for elf in self.elfs.iter()
         {
             self.field.insert(elf.pos, elf.id);
         }        
     }
 
-    fn neighbours(&self,p:Vec2)->usize
+    fn neighbours(&mut self,p:Vec2)->usize
     {
         let mut res = 0;
         for v in p.around8()
@@ -67,25 +71,112 @@ impl World {
         res
     }
 
+    fn move_to(p:Vec2,m:char)->Vec2
+    {        
+        match m {
+            'n'=> { Vec2::new(p.x  ,p.y-1) },
+            's'=> { Vec2::new(p.x  ,p.y+1) },
+            'w'=> { Vec2::new(p.x-1,p.y  ) },
+            'e'=> { Vec2::new(p.x+1,p.y  ) },
+            _ => panic!("wrong dir")
+        }    
+    }
+
     fn first_half(&mut self)
     {
-        for e in self.elfs.iter_mut()
+        //for e in self.elfs.iter_mut()
+        for i in 0..self.elfs.len()
         {
-            e.move_ok = true;
-            if self.neighbours(e.pos)==0
-            {
-                e.move_ok = false;
-            }
-
+            self.elfs[i].move_ok = true;
+            self.elfs[i].nothing = false;
         }
 
 
+        for i in 0..self.elfs.len()
+        {
+            self.elfs[i].move_ok = true;
 
+            if self.neighbours(self.elfs[i].pos)==0
+            {
+                self.elfs[i].move_ok = false;
+                self.elfs[i].nothing = true;
+            }
+        }
+
+        
+
+        for i in 0..self.elfs.len()
+        {
+            if self.elfs[i].move_ok
+            {
+                let mut good = false;
+
+                for m in self.elfs[i].moves.iter()
+                {
+                    if self.good_move(self.elfs[i].pos, *m)
+                    {
+                        self.elfs[i].move_to = Self::move_to(self.elfs[i].pos, *m);                        
+                        good = true;
+                        break;
+                    }
+                }
+
+                if !good
+                {
+                    self.elfs[i].move_ok = true;
+                }
+            }
+        }
+
+       // println!("{:#?}",self.elfs);
+
+        let mut cnt:HashMap<Vec2,usize> = HashMap::new();
+
+        for i in 0..self.elfs.len()
+        {
+            if self.elfs[i].move_ok
+            {
+                let c = cnt.get(&self.elfs[i].pos).unwrap_or(&0);
+                cnt.insert(self.elfs[i].pos, *c+1);
+                //(*cnt.get_mut(&self.elfs[i].move_to).unwrap_or(&mut 0))+=1;
+            }
+        }
+
+        println!("h:{:?}",cnt);
+
+        for i in 0..self.elfs.len()
+        {
+            if self.elfs[i].move_ok
+            {
+                let c = *cnt.get(&self.elfs[i].pos).unwrap_or(&0);
+
+                if c>1 
+                {
+                    self.elfs[i].move_ok = false;
+                }
+                //(*cnt.get_mut(&self.elfs[i].move_to).unwrap_or(&mut 0))+=1;
+            }
+
+        }
     }
 
     fn second_half(&mut self)
     {
-        
+        for e in self.elfs.iter_mut()
+        {
+            if e.move_ok && !e.nothing
+            {
+                e.pos = e.move_to;
+            }
+        }       
+
+        for e in self.elfs.iter_mut()
+        {
+            if !e.nothing
+            {
+                e.shift_moves();
+            }
+        }
     }
 
     fn moving(&mut self)
@@ -123,7 +214,7 @@ impl World {
                 self.is_empty(Vec2::new(p.x+1,p.y  )) &&
                 self.is_empty(Vec2::new(p.x+1,p.y+1))
             },
-            _ => panic!("wrong dir");
+            _ => panic!("wrong dir")
         }
     }
 
@@ -141,7 +232,7 @@ impl World {
                 if c=='#'
                 {
                     let pos = Vec2::new(px as i64,py as i64);
-                    elfs.push(Elf::new(pos, id))
+                    elfs.push(Elf::new(pos, id));
                                         
                     field.insert(pos, id);
                     id+=1;
@@ -155,23 +246,19 @@ impl World {
         }
     }
 
-  
-
-
-
     #[allow(unused)]
     fn print(&self,xx:usize,yy:usize)
     {
-        for y in 0..=yy
+        for y in -2..=yy as i64
         {
-            for x in 0..=xx
+            for x in -2..=xx as i64
             {
                 let pos = Vec2::new(x as i64,y as i64);                
                 let t = self.field.get(&pos);
 
                 if t.is_some() 
                 { 
-                    print!("#",t.unwrap().1); 
+                    print!("#"); 
                 }
                 else 
                 { 
@@ -202,9 +289,13 @@ impl World {
   
     fn compute(&mut self)->usize
     {    
+        self.print(11,11);
+
         for i in 0..5
         {
             self.moving();
+            println!("round {}",i);
+            self.print(11,11);
         }
         self.count_empty()
         /*
@@ -266,4 +357,19 @@ fn test1()
         "..............".to_string(),
     ];
     assert_eq!(part1(&v),110);
+}
+
+
+#[test]
+fn test2()
+{
+    let v = vec![
+                        ".....".to_string(),
+                        "..##.".to_string(),
+                        "..#..".to_string(),
+                        ".....".to_string(),
+                        "..##.".to_string(),
+                        ".....".to_string(),
+                        ];
+    assert_eq!(part1(&v),10);
 }
